cbd42ce603d920a68e3fbe8295167a1aecceca7b
singhsduos <ns9628491678@gmail.com>
2025-04-09 19:11:47 +0530

    Shift webpack and webpack-cli to dependencies

--- webpack.common.js
+++ webpack.common.js
@@ -6,8 +6,8 @@ module.exports = {
     app: "./src/index.js",
   },
   output: {
-    filename: "app.js", 
-    path: path.resolve(__dirname, "dist"), 
+    // filename: "app.js",
+    path: path.resolve(__dirname, "public"),
   },
   module: {
     rules: [
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,24 +1,20 @@
-const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-module.exports = merge(common, {
-  mode: "production",
-  devtool: false, // No source maps
-  output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "app.js",                   
-    clean: true,                       
+process.env["NODE_ENV"] = "production";
+
+module.exports = merge([
+  common,
+  {
+    mode: "production",
+    optimization: {
+      minimize: true,
+      minimizer: [
+        // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+        // `...`,
+        new CssMinimizerPlugin(),
+      ],
+    },
   },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      `...`, // Default minimizer (Terser)
-      new CssMinimizerPlugin()
-    ],
-  },
-  performance: {
-    hints: false,
-  },
-});
+]);
