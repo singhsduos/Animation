42692a1a2462c28d587a0856151461f49687a109
singhsduos <ns9628491678@gmail.com>
2025-04-09 18:44:20 +0530

    Shift webpack and webpack-cli to dependencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,26 +1,30 @@
+const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const path = require("path");
 
 process.env["NODE_ENV"] = "production";
 
 module.exports = merge(common, {
   mode: "production",
-  devtool: false, // removes source maps for smaller build
+  devtool: false,
+
+  output: {
+    path: path.resolve(__dirname, "dist"), 
+    filename: "app.js",                  
+    clean: true,                        
+    publicPath: "./",                   
+  },
+
   optimization: {
     minimize: true,
     minimizer: [
-      `...`, // keeps default minimizers like Terser
-      new CssMinimizerPlugin()
+      `...`, // keeps default JS minifier (TerserPlugin)
+      new CssMinimizerPlugin() // CSS minimizer
     ],
   },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'app.js',
-  },
+
   performance: {
-    hints: false, // disables bundle size warnings
-  },
+    hints: false, // disables performance warnings
+  }
 });
